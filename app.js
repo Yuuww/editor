e00422f54fbf8ed246fa8cb770df36c534b388e7
Yuu-Senpai <paul.fabian.bruhn@gmail.com>
2023-07-29 07:52:33 +0200

    many API Stuff

--- app.js
+++ app.js
@@ -6,57 +6,80 @@ let pressedTool = null;
 const toolFunctions = [toggleItems, openSettings];
 
 document.addEventListener('mouseup', (data) => {
-    if(hoverState != null && hoverState === pressedTool[1]) {
-        toolFunctions[pressedTool[1]]();
-        if(pressedTool[0].children[0].classList.contains('isPressed')) {
-            pressedTool[0].children[0].classList.remove('isPressed');
-        }
-    } else if(pressedTool != null && pressedTool[0].children[0].classList.contains('isPressed')) {
-            pressedTool[0].children[0].classList.remove('isPressed');
+  if(hoverState != null && hoverState === pressedTool[1]) {
+    toolFunctions[pressedTool[1]]();
+    if(pressedTool[0].children[0].classList.contains('isPressed')) {
+      pressedTool[0].children[0].classList.remove('isPressed');
     }
+  } else if(pressedTool != null && pressedTool[0].children[0].classList.contains('isPressed')) {
+    pressedTool[0].children[0].classList.remove('isPressed');
+  }
 });
 
 Array.from(workspace.children[0].getElementsByTagName('div')[0].getElementsByTagName('div')).forEach((tool, i) => {
-    tool.addEventListener('mousedown', (data) => {
-        pressedTool = [tool, i];
-        if(!tool.children[0].classList.contains('isPressed')) {
-            tool.children[0].classList.add('isPressed');
-        }
-    });
-    tool.addEventListener('mouseenter', (data) => {
-        hoverState = i;
-    });
-    
-    tool.addEventListener('mouseleave', (data) => {
-        hoverState = null;
-    });
+  tool.addEventListener('mousedown', (data) => {
+    pressedTool = [tool, i];
+    if(!tool.children[0].classList.contains('isPressed')) {
+      tool.children[0].classList.add('isPressed');
+    }
+  });
+  tool.addEventListener('mouseenter', (data) => {
+    hoverState = i;
+  });
+  tool.addEventListener('mouseleave', (data) => {
+    hoverState = null;
+  });
 });
 
-
-function sendRequest() {
-    const password = prompt('Bitte gib dein Passwort ein:');
-    if (!password) {
-        return;
-    }
-  
-    fetch('https://api.get-done.de:3001/login', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ password })
-    })
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-    })
-    .catch(error => {
-      console.error('Fehler bei der API-Anfrage:', error);
-    });
-}
-
 function toggleItems() {
-    workspace.children[0].classList.toggle('isClosed');
+  workspace.children[0].classList.toggle('isClosed');
 }
 
 function openSettings() {
-    sendRequest();
+  read();
+}
+
+let pw = null;
+
+// API Stuff
+function login() {
+  const password = prompt('Enter Passwort:');
+  if (!password) { return; }
+  fetch('https://api.get-done.de:3001/login', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ password })
+  })
+  .then(response => response.json())
+  .then(data => {
+    if(data.response === true) {
+      pw = password;
+      loadData();
+    } else {
+      pw = null;
+    }
+  })
+  .catch(error => {
+    console.error('Fehler bei der API-Anfrage:', error);
+  });
+}
+
+function read() {
+  if(pw === null) {
+    login();
+  }
+  fetch('https://api.get-done.de:3001/read', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ password })
+  })
+  .then(response => response.json())
+  .then(data => {
+    if(data.response === true) {
+      console.log(data);
+    }
+  })
+  .catch(error => {
+    console.error('Fehler bei der API-Anfrage:', error);
+  });
 }
